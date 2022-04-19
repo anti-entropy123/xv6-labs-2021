0bf9bf11a924d58820c2004a2ca82630318ce322
闹钟大魔王 <1348651580@qq.com>
2022-04-19 18:10:11 +0800

    pass sleep

--- user/sleep.c
+++ user/sleep.c
@@ -1,8 +1,38 @@
 #include "kernel/types.h"
-#include "kernel/stat.h"
 #include "user/user.h"
+// #include "user/ulib.c"
 
-int
-main(int argc, char *argv[]) {
+int user_sleep(int second)
+{
+    sleep(second);
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    int second;
+    char *s;
+    if (argc <= 1)
+    {
+        fprintf(2, "sleep: need sleep time\n");
+        exit(1);
+    }
+    s = argv[1];
+
+    while (1)
+    {
+        // fprintf(2, "current is %c\n", *s);
+        if (*s == '\0')
+            break;
+        if (!('0' <= *s && *s <= '9'))
+        {
+            fprintf(2, "sleep: wrong sleep time %s\n", argv[1]);
+            exit(1);
+        }
+        s++;
+    }
+    second = atoi(argv[1]);
+    fprintf(1, "you need sleep %d second", second);
+    user_sleep(second);
     exit(0);
 }

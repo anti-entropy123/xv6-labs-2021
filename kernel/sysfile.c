787d1856b97c6282943cee00015241b60be99c3e
闹钟大魔王 <1348651580@qq.com>
2022-06-05 09:53:48 +0800

    pass symbolic links

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -283,6 +283,35 @@ create(char *path, short type, short major, short minor)
   return ip;
 }
 
+
+uint64 sys_symlink(void) {
+  char target[MAXPATH], path[MAXPATH];
+  memset(target, 0, sizeof(target));
+  memset(path, 0, sizeof(path));
+  struct inode *ip;
+
+  if ( argstr(0, target, MAXPATH) < 0 || argstr(0, path, MAXPATH) < 0) {
+    return -1;
+  }
+  printf("[debug]: sys_symlink, target=%s, path=%s\n", target, path);
+
+  begin_op();
+  ip = create(path, T_SYMLINK, 0, 0);
+  if(ip == 0) {
+    end_op();
+    return -1;
+  }
+
+  if(writei(ip, 0, (uint64)target, 0, MAXPATH) != MAXPATH){
+    panic("symlink write failed");
+    return -1;
+  }
+
+  iunlockput(ip);
+  end_op();
+  return 0;
+}
+
 uint64
 sys_open(void)
 {
@@ -320,6 +349,29 @@ sys_open(void)
     iunlockput(ip);
     end_op();
     return -1;
+  }
+
+  if(ip->type == T_SYMLINK) {
+    if(!(omode & O_NOFOLLOW)) {
+      int cycle = 0;
+      char target[MAXPATH];
+      while(ip->type == T_SYMLINK) {
+        if(cycle == 10) {
+          iunlockput(ip);
+          end_op();
+          return -1;
+        }
+        cycle++;
+        memset(target, 0, sizeof(target));
+        readi(ip, 0, (uint64)target, 0, MAXPATH);
+        iunlockput(ip);
+        if((ip = namei(target)) == 0) {
+          end_op();
+          return -1;
+        }
+        ilock(ip);
+      }
+    }
   }
 
   if((f = filealloc()) == 0 || (fd = fdalloc(f)) < 0){
